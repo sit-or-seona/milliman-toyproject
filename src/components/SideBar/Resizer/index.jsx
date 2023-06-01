87ab00dc12d40e19ae0675be3e9156e78c0c7425
seona.bang <sit.or.seona@gmail.com>
2023-06-01 23:50:20 +0900

    feat: Resizable 컴포넌트(Category1)의 높이를 가용 최대 사이즈로 수정

--- src/components/SideBar/Resizer/index.jsx
+++ src/components/SideBar/Resizer/index.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Resizable } from "re-resizable";
 
 export default function Resizer({ children }) {
+  const [height, setHeight] = useState(0);
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+  window.onresize = () => {
+    setWindowHeight(window.innerHeight);
+  };
+
+  useEffect(() => {
+    setHeight(windowHeight - 76);
+  }, [windowHeight]);
+
   return (
     <Resizable
       enable={{
         bottom: true,
       }}
-      defaultSize={{ height: 277 }}
+      size={{ height: height }}
+      onResizeStop={(e, dir, ref, d) => {
+        setHeight(height + d.height);
+      }}
       handleStyles={{
         bottom: {
           bottom: 0,
